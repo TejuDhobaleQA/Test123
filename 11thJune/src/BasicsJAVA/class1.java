f6949e096206b81689718fc5b3a202d6c587fce5
Tejashri <Tejashri@DESKTOP-LLJ1QH7>
2022-11-03 15:46:49 +0530

    commit class1

--- 11thJune/src/BasicsJAVA/class1.java
+++ 11thJune/src/BasicsJAVA/class1.java
@@ -2,12 +2,6 @@ package BasicsJAVA;
 
 public interface class1 {
 	
-	default public void a() {
-		System.out.println("minal");
-	}
-	static public void b() {
-		System.out.println("patil");
-	}
 	
 	public static void main(String[] args) {
 		System.out.println("minal");
