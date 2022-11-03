938908fd6e0aa3edf656b605ed3ff46fbd081a18
Anant Dinesh Kurywanshi <91773552+Akurywanshi@users.noreply.github.com>
2022-11-03 15:42:05 +0530

    Update class1.java

--- 11thJune/src/BasicsJAVA/class1.java
+++ 11thJune/src/BasicsJAVA/class1.java
@@ -3,14 +3,15 @@ package BasicsJAVA;
 public interface class1 {
 	
 	default public void a() {
-		System.out.println("a");
+		System.out.println("minal");
 	}
 	static public void b() {
-		System.out.println("b");
+		System.out.println("patil");
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println("minal");
+		System.out.println("patil");
 	}
 
 }
